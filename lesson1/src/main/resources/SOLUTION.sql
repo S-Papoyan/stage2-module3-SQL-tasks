a4e6f99e19a9fec538273165d3bf0020f1dbb301
Sergey <spapoyan1987@gmail.com>
2023-02-03 19:58:18 +0400

    Commit3

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Student (id bigint Primary key, name varchar(255), birthday date, "group" int);
+CREATE TABLE Student (id bigint Primary key, name varchar(255), birthday date, groupnumber int);
 CREATE TABLE Subject (id bigint Primary key, name varchar(255), description varchar(255), grade int);
 CREATE TABLE PaymentType (id bigint Primary key, name varchar(255));
 CREATE TABLE Payment (id bigint Primary key, type_id bigint, foreign key(type_id) references PAYMENTTYPE (id), amount decimal, student_id bigint, foreign key (student_id) references STUDENT (id), payment_date datetime);
